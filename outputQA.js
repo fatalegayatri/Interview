e35d45926a44d558ad14645874f92b7fc8339c58
gayatri fatale <fatalegayatri24@gmail.com>
2024-12-04 19:00:50 +0530

    Add outputQA.js to demonstrate array mapping with boolean results

---
+++ outputQA.js
@@ -0,0 +1,13 @@
+const arr = [2, 3, 4, 5]
+const result = arr.map((item) => item % 2 === 0)
+console.log(result);
+// output= [ true, false, true, false]
+
+console.log(3 + "3");
+console.log(3 - "3");
+console.log([] + []);
+console.log([] - []);
+console.log({} + []);
+console.log({} - []);
+console.log({} + {});
+console.log({} - {});
